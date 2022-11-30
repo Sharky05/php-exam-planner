738b439591d8da39c199cd48ce767e2392f940f3
Sharky05 <91487768+Sharky05@users.noreply.github.com>
2022-11-30 09:06:47 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,26 @@
+<!DOCTYPE html>
 
+<html lang="nl">
+  
+  <head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Website</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="site_icon_primary.png">
+    
+  </head>
+  
+  <body>
+    
+    <main>
+      
+        <h1>Welcome to My Website</h1>
+        
+    </main>
+    
+  </body>
+  
+</html>
