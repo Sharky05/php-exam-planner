1a46e31873ea811462c27c80e1de1a57d53c29d0
Sharky05 <91487768+Sharky05@users.noreply.github.com>
2022-11-30 10:34:29 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,1 +1,43 @@
+<!DOCTYPE html>
 
+<html lang="nl">
+  
+  <head>
+    
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Information</title>
+    <link rel="stylesheet" href="./style.css">
+    <link rel="icon" href="site_icon_primary.png">
+    
+  </head>
+  
+  <body>
+    
+    <main>
+      <button type="button"
+              id="home"
+              onclick="location.href = 'index.php'">
+         Home
+      </button>
+       <h1>Contact Informatie</h1>
+      
+      <h2>
+        Nico Francis - Developer
+      </h2>
+    <p>Zakelijke Email: nfrancis@ogvo.nl<br>
+    Telefoonnummer: +31 6 11156150</p>
+      
+      <h2>
+        Stan Obers - Developer
+      </h2>
+      <p>Zakelijke Email: stobers@ogvo.nl<br>
+        Telefoonnummer: +31 6 41907731
+      </p>
+      
+    </main>
+    
+  </body>
+  
+</html>
