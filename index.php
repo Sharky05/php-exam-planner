18afb502d2c59686342892903ede378b1d88d548
Sharky05 <91487768+Sharky05@users.noreply.github.com>
2023-01-16 15:24:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Website</title>
+    <title>Homepage</title>
     <link rel="stylesheet" href="./style.css">
     <link rel="icon" href="site_icon_primary.png">
     
